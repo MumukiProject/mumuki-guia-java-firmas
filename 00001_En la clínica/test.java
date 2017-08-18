1423bd3565ac6af90e367116e75af71f1b43a0d0
franco@mumuki.org <franco@mumuki.org>
2017-08-18 14:28:18 -0400

    Mumuki Export on 2017-08-18 14:28:18 -0400

--- 00001_En la clínica/test.java
+++ 00001_En la clínica/test.java
@@ -1,4 +1,5 @@
-static void main(String[] args) {
+class EjemploDeUso {
+  static void main(String[] args) {
     Gato gardfield = new Gato();
     ClinicaDeMascotas laClinica = new ClinicaDeMascotas();
     laClinica.atender(gardfield, 5);
