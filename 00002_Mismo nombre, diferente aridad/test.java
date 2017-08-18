516098383b23d1d8857c869d52df99defce008e9
franco@mumuki.org <franco@mumuki.org>
2017-08-18 15:08:33 -0400

    Mumuki Export on 2017-08-18 15:08:33 -0400

--- 00002_Mismo nombre, diferente aridad/test.java
+++ 00002_Mismo nombre, diferente aridad/test.java
@@ -2,7 +2,7 @@ static class EjemploDeUso {
   static void main(String[] args) {
     Gato gardfield = new Gato();
     ClinicaDeMascotas laClinica = new ClinicaDeMascotas();
-    laClinica.atender(gardfield, 5);
+    laClinica.internar(gardfield, 5);
   }
 }
 
