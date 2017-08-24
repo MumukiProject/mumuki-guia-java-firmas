846686080d46f09350ea21025f9b63b7670e463f
franco@mumuki.org <franco@mumuki.org>
2017-08-24 16:41:47 -0400

    Mumuki Export on 2017-08-24 16:41:47 -0400

---
+++ 00004_Como perros y gatos/extra.java
@@ -0,0 +1,25 @@
+class Gato {
+  void alimentar() {
+    // ...
+  }
+  void peinar() {
+    // ...
+  }
+  void cortarUnias() {
+    // ...
+  }
+}
+
+class Perro {
+  void alimentar() {
+    // ...
+  }
+  
+  void jugar() {
+    // ...
+  }
+  
+  void correr() {
+    // ...
+  }
+}
--- 00004_Como perros y gatos/test.java
+++ 00004_Como perros y gatos/test.java
@@ -1,1 +1,11 @@
-TODO
+@Test
+public void se_puede_internar_un_perro() {
+  ClinicaDeMascotas clinica = new ClinicaDeMascotas();
+  clinica.atender(new Perro());
+}
+
+@Test
+public void se_puede_internar_un_gato() {
+  ClinicaDeMascotas clinica = new ClinicaDeMascotas();
+  clinica.atender(new Gato());
+}
