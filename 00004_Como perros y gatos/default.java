91315b0db0d93756bc7188aef5858f96206c3a40
franco@mumuki.org <franco@mumuki.org>
2017-08-24 16:42:36 -0400

    Mumuki Export on 2017-08-24 16:42:36 -0400

--- 00004_Como perros y gatos/default.java
+++ 00004_Como perros y gatos/default.java
@@ -1,1 +1,14 @@
-/*...previousContent...*/
+class ClinicaDeMascotas {
+  void atender(Gato unPaciente) {
+    unPaciente.alimentar();
+    unPaciente.peinar();
+    unPaciente.cortarUnias();
+  }
+  
+  void internar(Gato unPaciente, int dias) {
+    IntStream
+      .range(1, dias)
+      .forEach(dia -> atender(unPaciente));
+  }
+}
+
