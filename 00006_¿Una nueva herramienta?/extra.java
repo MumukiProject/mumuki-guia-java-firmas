7bdc3e3ab31410d3da1d96fc38c2acdd29aed7e1
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-02-25 13:48:56 -0500

    Mumuki Export on 2018-02-25 13:48:56 -0500

--- 00006_¿Una nueva herramienta?/extra.java
+++ 00006_¿Una nueva herramienta?/extra.java
@@ -1,21 +1,21 @@
 interface Numero {
-  double costoPorLlamar();
+  double costoPorSegundo();
 }
 
 class NumeroLocal implements Numero {
-  public double costoPorLlamar() {
+  public double costoPorSegundo() {
     return 0.02;
   }
 }
 
 class NumeroNacional implements Numero {
-  public double costoPorLlamar() {
+  public double costoPorSegundo() {
     return 0.07;
   }
 }
 
 class NumeroInternacional implements Numero {
-  public double costoPorLlamar() {
+  public double costoPorSegundo() {
     return 0.13;
   }
 }
