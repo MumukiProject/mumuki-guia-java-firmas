21ea84b144090f7cfd7d955951f4cee93d62836a
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-02-25 13:57:05 -0500

    Mumuki Export on 2018-02-25 13:57:05 -0500

--- 00008_Pica pica/test.java
+++ 00008_Pica pica/test.java
@@ -1,14 +1,14 @@
 @Test
 public void un_ataque_electrico_no_es_super_efectivo_contra_un_monstruito_fuego() {
-  Assert.assertFalse(ataqueElectrico.esSuperEfectivo(new MonstruitoFuego());
+  Assert.assertFalse(ataqueElectrico.esSuperEfectivo(new MonstruitoFuego()));
 }
 
 @Test
 public void un_ataque_electrico_no_es_super_efectivo_contra_un_monstruito_tierra() {
-  Assert.assertFalse(ataqueElectrico.esSuperEfectivo(new MonstruitoTierra());
+  Assert.assertFalse(ataqueElectrico.esSuperEfectivo(new MonstruitoTierra()));
 }
 
 @Test
 public void un_ataque_electrico_es_super_efectivo_contra_un_monstruito_agua() {
-  Assert.assertTrue(ataqueElectrico.esSuperEfectivo(new MonstruitoAgua());
+  Assert.assertTrue(ataqueElectrico.esSuperEfectivo(new MonstruitoAgua()));
 }
