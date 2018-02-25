fbb34b0e7341f03515ccc3dd651060f80517b050
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-02-25 13:48:03 -0500

    Mumuki Export on 2018-02-25 13:48:03 -0500

--- 00006_¿Una nueva herramienta?/test.java
+++ 00006_¿Una nueva herramienta?/test.java
@@ -5,15 +5,15 @@ Telefono telefono = new Telefono();
 
 @Test
 public void el_costo_de_una_llamada_local_es_de_dos_centavos() {
-  Assert.assertEquals(2, telefono.costoPorLlamar(numeroLocal, 100));
+  Assert.assertEquals(2, telefono.costoPorLlamar(numeroLocal, 100), 0);
 }
 
 @Test
 public void el_costo_de_una_llamada_nacional_es_de_siete_centavos() {
-  Assert.assertEquals(7, telefono.costoPorLlamar(numeroNacional, 70));
+  Assert.assertEquals(7, telefono.costoPorLlamar(numeroNacional, 70), 0);
 }
 
 @Test
 public void el_costo_de_una_llamada_internnacional_es_de_trece_centavos() {
-  Assert.assertEquals(13, telefono.costoPorLlamar(numeroInternacional, 13));
+  Assert.assertEquals(13, telefono.costoPorLlamar(numeroInternacional, 13), 0);
 }
